712268dd863840b51bfc5694264768e1aa3cf288
Djair Silva <djavasilva@yahoo.com.br>
2024-11-15 11:35:31 -0300

    Banco de dados

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "green" : "magenta"}
+          color={showAdd ? "gray" : "yellow"}
           text={showAdd ? "Fechadinhuu" : "Criarzinho"}
           onClick={onAdd}
         />
